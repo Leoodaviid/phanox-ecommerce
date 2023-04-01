014b2123db6cb7bcd613f73bed48ace4bff1c176
Leoodaviid <david_ax7@hotmail.com>
2023-03-31 22:46:05 -0300

    feat: page Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
-'use client'
+// 'use client'
+import { Product, FooterBanner, HeroBanner } from '@/components'
+import { getBanner, getProducts } from '@/lib/api'
+import styles from '../styles'
 
-const Home = () => {
+const Home = async () => {
+  const productsData = await getProducts({ cache: 'no-store' })
+  const bannerData = await getBanner({ cache: 'no-store' })
+
   return (
     <>
-      <div>
-        <h2>Produtos mais vendidos</h2>
-        <p>Fones de ouvido variados</p>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className='text-center my-8 text-[#324d67]'>
+        <h2 className='text-4xl font-extrabold'>Produtos mais vendidos</h2>
+        <p className='text-xs font-extralight'>Fones de ouvido variados</p>
       </div>
-      <div>{['Product 1', 'Product 2', 'Product 3'].map((product) => product)}</div>
+      <div className={`${styles.flexCenter} w-full flex-wrap gap-4`}>
+        {productsData?.map((product) => product.name)}
+        <br />
+        {bannerData?.map((banner) => banner.discount)}
+      </div>
+      <FooterBanner />
     </>
   )
 }
