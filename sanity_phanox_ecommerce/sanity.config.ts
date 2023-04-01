cf83a474649c90c98d5b213a9653a8187e950eea
Leoodaviid <david_ax7@hotmail.com>
2023-03-31 22:39:47 -0300

    build: dependencies

--- sanity_phanox_ecommerce/sanity.config.ts
+++ sanity_phanox_ecommerce/sanity.config.ts
@@ -8,7 +8,7 @@ const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
-  title: 'cornsilk-hamster',
+  title: 'sanity_phanox_ecommerce',
 
   projectId: '1f4i3o24',
   dataset: 'production',
@@ -19,4 +19,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
-
