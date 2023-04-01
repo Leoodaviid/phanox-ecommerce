61a29bb0b7fb913e05fc7af0aaaaa1ccd07fe849
Leoodaviid <david_ax7@hotmail.com>
2023-04-01 20:44:20 -0300

    feat: Navbar & Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
-'use client'
-import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <div className='flex justify-center flex-col gap-2.5 font-bold text-[#324d67] text-center mt-5 py-7 px-2.5'>
+      <p>2023 PHANOX&copy; Alguns direitos reservados.</p>
+      <p className='flex justify-center text-xl gap-2.5'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
 }
 
 export default Footer
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
-'use client'
-import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='flex justify-between relative my-1.5 mx-4'>
+      <p className='text-stone-400 text-lg'>
+        <Link href='/'>PHANOX</Link>
+      </p>
+      <button
+        type='button'
+        className='text-2xl text-stone-400 cursor-pointer relative transition-transform duration-300 ease-in hover:scale-125'
+      >
+        <AiOutlineShopping />
+        <span className='absolute top-0 -right-2 text-xs text-[#eee] bg-[#f02d34] w-[18px] h-[18px] rounded-full text-center font-semibold'>
+          1
+        </span>
+      </button>
+    </div>
+  )
 }
 
 export default Navbar
