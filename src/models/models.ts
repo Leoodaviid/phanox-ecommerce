6687b0d8ae033694862dec730dfc92cea46b3cbc
Leoodaviid <david_ax7@hotmail.com>
2023-03-31 22:44:49 -0300

    feat: models data

---
+++ src/models/models.ts
@@ -0,0 +1,20 @@
+export interface ProductsData {
+  image: string[]
+  name: string
+  slug: string
+  price: number
+  details: string
+}
+
+export interface BannerData {
+  image: string
+  buttonText: string
+  product: string
+  desc: string
+  smallText: string
+  midText: string
+  largeText1: string
+  largeText2: string
+  discount: string
+  saleTime: string
+}
