5e88550b2dc306287610551da6cd4b497b27d4b4
Leoodaviid <david_ax7@hotmail.com>
2023-03-31 22:47:00 -0300

    feat: component HeroBanner

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,30 +1,37 @@
-'use client'
+// 'use client'
 import Link from 'next/link'
 import styles from '../styles'
+import { BannerData } from '@/models/models'
+import { urlFor } from '@/lib/client'
 
-const HeroBanner = () => {
+interface HeroBannerProps {
+  heroBanner: BannerData
+}
+
+const HeroBanner = ({ heroBanner }: HeroBannerProps) => {
   return (
     <div className={`${styles.paddings} w-full h-[500px] relative bg-[#dcdcdc] rounded-[15px]`}>
       <div>
-        <p className='text-sm'>SMALL TEXT</p>
-        <h3 className='text-6xl font-semibold mt-[4px]'>MID TEXT</h3>
+        <p className='text-sm'>{heroBanner.smallText}</p>
+        <h3 className='text-6xl font-bold mt-[4px]'>{heroBanner.midText}</h3>
+        <h1 className='text-white text-[10em] upercase -ml-5'>{heroBanner.largeText1}</h1>
         <img
-          src=''
-          alt='headphones'
+          src={urlFor(heroBanner.image).url()}
+          alt='headphone'
           className='absolute top-[0%] right-[20%] w-[450px] h-[450px]'
         />
         <div>
-          <Link href='/product/ID'>
+          <Link href={`/product/${heroBanner.product}`}>
             <button
               type='button'
               className='cursor-pointer bg-[#f02d34] rounded-[15px] py-1 px-4 my-10 text-lg font-medium text-white z-10 !important'
             >
-              BUTTON TEXT
+              {heroBanner.buttonText}
             </button>
           </Link>
           <div className='absolute right-[10%] bottom-[5%] w-[300px] flex flex-col text-[#324d67]'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
