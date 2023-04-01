2db497bf07667d4a1a14d89ea29291da008f48b1
Leoodaviid <david_ax7@hotmail.com>
2023-04-01 20:00:19 -0300

    feat: website layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,25 @@
-'use client'
-
-const Layout = () => {
-  return <div>Layout</div>
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { ReactNode } from 'react'
+interface LayoutProps {
+  children: ReactNode
+}
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className='p-2.5'>
+      <Head>
+        <title>Phanox Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='max-w-[1400px] w-full m-auto'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
