2d0e15b6101cfe071292a0e67e48a507bf3dd7ea
Leoodaviid <david_ax7@hotmail.com>
2023-04-03 09:42:58 -0300

    feat: new search for specific product

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,10 @@
 import { BannerData, ProductsData } from '@/models/models'
 import { client } from './client'
-import { bannerQuery, productsQuery } from './queries'
+import { bannerQuery, productsQuery, slugProductQuery } from './queries'
 
 export const getProducts = client.createApiUtil<ProductsData[]>(productsQuery)
+
+export const getProductsSlug = (slug: string) =>
+  client.createApiUtil<ProductsData>(slugProductQuery(slug))
 
 export const getBanner = client.createApiUtil<BannerData[]>(bannerQuery)
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,4 +2,7 @@ import { groq } from 'next-sanity'
 
 export const productsQuery = groq`*[_type == "product"]`
 
+export const slugProductQuery = (slug: string) =>
+  groq`*[_type == "product" && slug.current == '${slug}'][0]`
+
 export const bannerQuery = groq`*[_type == "banner"]`
