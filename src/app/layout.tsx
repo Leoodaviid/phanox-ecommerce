25e7807a0f097284560da5739de7d61b83b46788
Leoodaviid <david_ax7@hotmail.com>
2023-04-03 16:17:25 -0300

    feat: passing context to application + Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+'use client'
 import { Layout } from '@/components'
 import { ReactNode } from 'react'
+import { StateContext } from '@/context/StateContext'
+import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 
 export const metadata = {
@@ -11,7 +14,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='pt-BR'>
       <body>
-        <Layout>{children}</Layout>
+        <StateContext>
+          <Toaster />
+          <Layout>{children}</Layout>
+        </StateContext>
       </body>
     </html>
   )
