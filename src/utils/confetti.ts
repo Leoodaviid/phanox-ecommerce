f182279fe4d43ef1a81735f3ceeda48529e5b44d
Leoodaviid <david_ax7@hotmail.com>
2023-04-07 11:48:04 -0300

    feat: confetti

---
+++ src/utils/confetti.ts
@@ -0,0 +1,32 @@
+import confetti from 'canvas-confetti'
+
+export const runFireWorks = () => {
+  let duration = 5 * 1000
+  let animationEnd = Date.now() + duration
+  let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+  function randoInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min
+  }
+
+  let interval: NodeJS.Timeout = setInterval(function () {
+    let timeLeft = animationEnd - Date.now()
+
+    if (timeLeft <= 0) return clearInterval(interval)
+
+    let particleCount = 50 * (timeLeft / duration)
+
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randoInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      }),
+    )
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randoInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      }),
+    )
+  }, 250)
+}
